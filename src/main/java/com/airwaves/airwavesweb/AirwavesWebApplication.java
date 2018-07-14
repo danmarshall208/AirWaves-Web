6ec65091be23c2cea67d6e32f7c4452680665a76
Daniel Marshall <danmarshall208@hotmail.com>
2018-07-15 01:33:17 +1000

    First code

---
+++ src/main/java/com/airwaves/airwavesweb/AirwavesWebApplication.java
@@ -0,0 +1,12 @@
+package com.airwaves.airwavesweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirwavesWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AirwavesWebApplication.class, args);
+    }
+}
